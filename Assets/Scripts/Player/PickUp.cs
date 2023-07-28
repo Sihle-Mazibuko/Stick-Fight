fc8fc36e7120a4bac42e3d87a3d3e39420c7d7a3
Sihle-Mazibuko <sihlemazii@gmail.com>
2023-07-28 09:44:33 +0200

    Change

--- Assets/Scripts/Player/PickUp.cs
+++ Assets/Scripts/Player/PickUp.cs
@@ -4,40 +4,38 @@ using UnityEngine;
 
 public class PickUp : MonoBehaviour
 {
-    [SerializeField] Transform weaponHolder;
+    [SerializeField] 
+    Transform weaponHolder;
     GameObject weapon;
-    public Vector3 Direction { get; set; }
+    Vector3 Direction = new Vector3(3,0,0); 
 
+    [SerializeField]
+    private LayerMask gunMask;
 
-
-    private void Update()
+    private void FixedUpdate()
     {
-        weapon = GameObject.FindGameObjectWithTag("Weapon");
-        Debug.Log(weapon.name);
 
-        if (Input.GetKeyDown(KeyCode.P))
+        if (Input.GetKeyDown(KeyCode.F) || Input.GetKeyDown(KeyCode.B))
         {
-            PickUpWeapon();
+            ThrowWeapon();
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        //if(collision.gameObject.tag == "Weapon")
-        //{
-        //    PickUpWeapon();
-        //}
+        if (collision.gameObject.tag == "Weapon")
+        {
+            PickUpWeapon();
+        }
     }
 
     void PickUpWeapon()
     {
-        if (weapon)
+        if (weapon == null)
         {
+            Collider2D weaponItem = Physics2D.OverlapCircle(transform.position, 1, gunMask.value);
+            Debug.Log(weaponItem.gameObject.name);
 
-        }
-        else
-        {
-            Collider2D weaponItem = Physics2D.OverlapCircle(transform.position, 1);
             if (weaponItem)
             {
                 weapon = weaponItem.gameObject;
@@ -49,6 +47,20 @@ public class PickUp : MonoBehaviour
                 }
             }
 
+        }
+    }
+
+    void ThrowWeapon()
+    {
+        if (weapon != null)
+        {
+            weapon.transform.position = transform.position + Direction;
+            weapon.transform.parent = null;
+            if (weapon.GetComponent<Rigidbody2D>())
+            {
+                weapon.GetComponent<Rigidbody2D>().simulated = true;
+            }
+            weapon = null;
         }
     }
 }
