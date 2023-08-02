f6d6f6b6c6793cc06ad884034aa0e65d58c447f2
Uyalrin <daniel.wolfbat@gmail.com>
2023-08-02 16:07:02 +0200

    c

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -35,7 +35,7 @@ public class Movement : MonoBehaviour
     {
         horizontal = Input.GetAxisRaw("Horizontal");
 
-        if (Input.GetButtonDown("Jump")) //&& isGrounded())
+        if (Input.GetButtonDown("Jump") && isGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpPower);
             Debug.Log("Jump");
